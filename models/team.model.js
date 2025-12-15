c72077201d4e8c3f5fcfe454f09d58afffa1751a
nihalukare <nihalukare959@gmail.com>
2025-12-15 18:13:37 +0530

    added members field to the team schema

--- models/team.model.js
+++ models/team.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 // Team Schema
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true }, // Team names must be unique
+  members: { type: [String] },
   description: { type: String }, // optional description for the team
 });
 
